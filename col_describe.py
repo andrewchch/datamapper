57bbff5b770bdfe4f172d3f551b9d1a3fcf2dd1d
andrewchch <andrewjgroom@gmail.com>
2023-05-31 11:39:59 +1200

    Added splitting of concatenated fields.

--- col_describe.py
+++ col_describe.py
@@ -7,6 +7,10 @@ import sys
 from tqdm import tqdm
 
 clean_re = re.compile('[\\[\\]\\:\\/\\?\\*]')
+MULTIVALUE_COLUMN_DELIM = '[\\[\\]\:]+'
+SUBROWS_RE = re.compile('\\[(.*?)\\]')
+FIELD_DELIM_RE = re.compile('(?<!\s):(?!\s)')
+multivalue_column_delim_re = re.compile(MULTIVALUE_COLUMN_DELIM)
 
 
 # Load Excel file into pandas dataframe
@@ -37,7 +41,8 @@ def add_describe_sheets(file_path, _describe_results):
     # Add two sheets for numeric and object fields
     all_rows = []
 
-    for column, results in tqdm(_describe_results.items(), desc="Building describe results", unit="item"):
+    for column in tqdm(sorted(_describe_results.keys()), desc="Building describe results", unit="item"):
+        results = _describe_results[column]
         all_rows.append(results)
 
     merged_df = pd.DataFrame(all_rows)
@@ -45,6 +50,22 @@ def add_describe_sheets(file_path, _describe_results):
     merged_df.to_excel(writer, sheet_name='merged fields')
 
     writer.close()
+
+
+def parse_col(row, col):
+    if pd.isna(row[col]):
+        return None
+
+    # Find all bracketed text matches in the column
+    matches = SUBROWS_RE.findall(row[col])
+    # Split each match on ':' and convert to DataFrame
+    new_df = pd.DataFrame([FIELD_DELIM_RE.split(m) for m in matches])
+    # Assign column names based on the original column name
+    col_matches = SUBROWS_RE.split(col)
+    sub_cols = FIELD_DELIM_RE.split(col_matches[1])
+    col_prefix = col_matches[0].strip()
+    new_df.columns = ['%s_%s' % (col_prefix, x) for x in sub_cols]
+    return new_df
 
 
 def main():
@@ -61,6 +82,25 @@ def main():
     # Process the file
     sys.stdout.write('Loading file..')
     df = load_excel_to_dataframe(tgt)
+
+    # Further split columns that contain delimited values into additional
+    # Iterate over columns to find ones with bracketed sub-column names
+    for col in tqdm(df.columns, desc="Expanding delimited columns", unit="column"):
+        if ' [' in col and ':' in col and ']' in col:
+            # Parse this column into a list of DataFrames
+            dfs = []
+            for _, row in df.iterrows():
+                _new_df = parse_col(row, col)
+                if _new_df is not None:
+                    dfs.append(_new_df)
+
+            # Concatenate these DataFrames into one
+            if len(dfs) > 0:
+                parsed_df = pd.concat(dfs)
+                # Reset the index
+                parsed_df.reset_index(drop=True, inplace=True)
+                # Add the parsed DataFrame to the original DataFrame, dropping the original column
+                df = pd.concat([df, parsed_df], axis=1).drop(columns=col)
 
     sys.stdout.write('Getting describe results..')
     describe_results = get_describe_results(df)
